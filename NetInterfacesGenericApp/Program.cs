0b6a2a564e20a9309929413d56dd5c989dec14c3
Ефимов Максим <efimov_m@itstep.academy>
2023-04-02 15:10:33 +0300

    Добавьте файлы проекта.

---
+++ NetInterfacesGenericApp/Program.cs
@@ -0,0 +1,66 @@
+﻿Messanger<EmailMessage> messanger = new Messanger<EmailMessage>();
+
+
+class Car
+{
+
+}
+interface IMessage
+{
+    string Text { set; get; }
+}
+
+interface IPrintable
+{
+    void Print();
+}
+
+class Message : IMessage, IPrintable
+{
+    public string Text { get; set; }
+
+    public Message() { }
+    public Message(string text)
+    {
+        Text = text;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(Text);
+    }
+}
+
+class EmailMessage : Message
+{
+
+}
+
+class SmsMessage : Message
+{
+
+}
+
+class Messanger<T> where T : IMessage, IPrintable
+{
+    public void Send(T message)
+    {
+        Console.WriteLine(message);
+        message.Print();
+    }
+}
+
+interface IId<T>
+{
+    T Id { set; get; }
+}
+
+class Person<T> : IId<T>
+{
+    public T Id { get; set; }
+}
+
+class PersonIntId : IId<int>
+{
+    public int Id { get; set; }
+}
